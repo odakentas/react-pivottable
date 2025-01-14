20d1599b3ac43ab1850ddf2d9f48a5f9ca735eb8
Abdulkadir <abdulkadir.ozturk@odakent.com.tr>
2025-01-14 11:33:00 +0300

    math min max optimized

--- src/TableRenderers.jsx
+++ src/TableRenderers.jsx
@@ -50,8 +50,8 @@ const spanSize = function (arr, i, j, no_loop = false) {
 };
 
 function redColorScaleGenerator(values) {
-    const min = Math.min.apply(Math, values);
-    const max = Math.max.apply(Math, values);
+    const min = Math.min(...values);
+    const max = Math.max(...values);
     return (x) => {
         const nonRed = 255 - Math.round((255 * (x - min)) / (max - min));
         return { backgroundColor: `rgb(255,${nonRed},${nonRed})` };
